801bbeb0cf9889a65401fddb0579d281f76af728
CrossR <r.cross@lancaster.ac.uk>
2022-04-21 22:41:27 +0100

    Test out using macros to remove some of the duplication in the keymap.

---
+++ config/sofle.h
@@ -0,0 +1,19 @@
+// Common and base level macros.
+
+// Convenient macros to make keymaps easier to read.
+#define XXXXX &none
+#define ______ &trans
+
+// Layer names
+#define BASE 0
+#define RAISE 1
+#define LOWER 2
+#define GAME 3
+#define GAME_RAISE 4
+#define MAC 5
+
+// BT Profile numbers
+#define PC_BT 1
+#define MAC_BT 2
+#define PHONE_BT 3
+#define TABLET_BT 4
